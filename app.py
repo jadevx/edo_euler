1c27ebb2849e3764260b7ccf00bea36ff66c2ac4
jadevx <93341469+jadevx@users.noreply.github.com>
2025-04-10 17:49:21 -0500

    Mejoras generales

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def index():
     return render_template('index.html', puntos=None)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5001)
